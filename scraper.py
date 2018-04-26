7e4344ce8ad50d9b266a13894b4cd8353d2953f1
Rebecca Ackerman <rcackerman@gmail.com>
2018-04-26 11:40:55 -0400

    Trying to restart browser after failure

--- scraper.py
+++ scraper.py
@@ -11,7 +11,18 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def parse_judge_page(source):
+def scrape_alphabet_list(driver, letter):
+    """Use the letter given to determine what directory page to go to,
+    then scrape and return a list of each judge on the page
+    """
+    browser.find_element_by_link_text(letter).click()
+
+    # Each alphabetized page has links to the judges, which we will record
+    _ = browser.find_elements_by_xpath("//a[contains(@href, 'JUDGE_ID')]")
+    return [i.text for i in _]
+
+
+def section_judge_page(source):
     """Get the main section and clean empty paragraphs.
     Returns a bs4 tag object.
     """
@@ -46,53 +57,83 @@ def get_court(court_string):
     return([s for s in court_string.stripped_strings][0])
     
 
-# Start the browser
-browser = webdriver.Firefox()
-browser.implicitly_wait(60)
+def parse_judge_page(source):
+    """Parse the judge detail page
+    """
+    outdict = {}
+    page = section_judge_page(source)
+    sections = [h2.text for h2 in page.find_all('h2')]
 
-JUDGES = {}
+    # Get the court
+    _ = get_generic_section_text(page, re.compile('Hon.'))
+    outdict['court'] = [get_court(court) for court in _]
 
-browser.get("https://iapps.courts.state.ny.us/judicialdirectory/JudicialDirectory")
+    # Parse the other sections
+    for section in sections: # ignore the first part
+        if 'Hon.' not in section:
+            _ = get_generic_section_text(page, section)
+            outdict[section] = [t.text for t in _]
+        else:
+            pass
+
+    return outdict
+
+
+# Try to load judges from the existing judges file
+try:
+    with open('judges.json', 'r') as f:
+        JUDGES = json.load(f)
+except:
+    JUDGES = {}
+print(JUDGES.keys())
+
 # element = WebDriverWait(browser, 10).until(
                 # EC.presence_of_element_located((By.ID, "column1_A630")))
 
 # # For each letter in the alphabet, click on the link
 for letter in list(string.ascii_uppercase):
-    print(letter)
-    browser.find_element_by_link_text(letter).click()
-
-    # Each alphabetized page has links to the judges, which we will record
-    _ = browser.find_elements_by_xpath("//a[contains(@href, 'JUDGE_ID')]")
-    pagejudges = [i.text for i in _]
-    print(pagejudges)
+    # Start the browser
+    browser = webdriver.Firefox()
+    browser.implicitly_wait(60)
+    browser.get("https://iapps.courts.state.ny.us/judicialdirectory/JudicialDirectory")
+    pagejudges = scrape_alphabet_list(browser, letter)
 
     # TODO: fail gracefully by quitting and restarting
     # maybe pop judges so that if one fails it'll go again
     for judge in pagejudges:
-        judge_url = browser.find_element_by_link_text(judge).get_attribute('href')
+        if judge not in JUDGES.keys():
+            try:
+                judge_url = browser.find_element_by_link_text(judge).get_attribute('href')
+                # Append the judge's name +  url for the judge
+                JUDGES[judge] = {'judge url': judge_url}
+                time.sleep(10)
 
-        browser.find_element_by_link_text(judge).click()
-        time.sleep(10)
-        judge_page = parse_judge_page(browser.page_source)
+                browser.find_element_by_link_text(judge).click()
+                judge_info = parse_judge_page(browser.page_source)
 
-        # Append the judge's name +  url for the judge
-        JUDGES[judge] = {'judge url': judge_url}
+                # Update judge dict with all sections
+                JUDGES[judge].update(judge_info)
+                time.sleep(15)
+                browser.back()
+                print(judge + " detail page scraped")
+            except:
+                browser.quit()
+                browser = webdriver.Firefox()
+                browser.implicitly_wait(60)
+                browser.get("https://iapps.courts.state.ny.us/judicialdirectory/JudicialDirectory")
+                browser.find_element_by_link_text(letter).click()
+                browser.find_element_by_link_text(judge).click()
+                judge_info = parse_judge_page(browser.page_source)
 
-        # Update judge dict  with all sections
-        sections = [h2.text for h2 in judge_page.find_all('h2')]
-        for section in sections[1:]: # ignore the first part
-            _ = get_generic_section_text(judge_page, section)
-            JUDGES[judge][section] = [t.text for t in _]
-        
+                # Update judge dict with all sections
+                JUDGES[judge].update(judge_info)
+                time.sleep(15)
+                browser.back()
+                print(judge + " detail page scraped")
+        else:
+            pass
 
-        # Get courts
-        _ = get_generic_section_text(judge_page, re.compile('Hon.'))
-        courts = [get_court(court) for court in _]
-        time.sleep(15)
-        browser.back()
-        print(judge + " detail page scraped")
-
-browser.quit()
+    browser.quit()
 
 with open('judges.json', 'w') as f:
     f.write(json.dumps(JUDGES))
